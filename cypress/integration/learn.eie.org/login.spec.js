5ede6d7cdecf04e68333ff1fe6f3b2db2ba54d3d
Chris Chapman <chrischapman900@gmail.com>
2022-01-05 16:30:52 -0500

    Add example env and remove videos

--- cypress/integration/learn.eie.org/login.spec.js
+++ cypress/integration/learn.eie.org/login.spec.js
@@ -1,35 +1,37 @@
 /// <reference types="cypress" />
 
-describe('log in to dashboard', () => {
+context('logged in', () => {
+  before(() => {
+    cy.visit('https://learn.eie.org')
+  })
 
-    before(() => {
-      cy.visit('https://learn.eie.org')
-    })
+  it('shows the login screen', () => {
+    cy.get('[formcontrolname=username]').should('be.visible')
+  })
 
-    it('shows the login screen'), () => {
-      cy.get('formcontrolname=username').should('be.visible')
+  it('can log in', {
+    defaultCommandTimeout: 60000
+  }, () => {
+    const username = Cypress.env('username');
+    const password = Cypress.env('password');
+
+    cy.get('[formcontrolname=username]').type(username)
+    cy.get('[formcontrolname=password]').type(password)
+
+    cy.contains('Log In').click()
+
+    if (cy.get('.swal2-actions', {timeout: 5000})){
+      cy.get('.swal2-actions').contains('Continue').click()
     }
-  
-    it('can log in', {
-      defaultCommandTimeout: 60000
-    }, () => {
-      const username = Cypress.env('username');
-      const password = Cypress.env('password');
-  
-      cy.get('[formcontrolname=username]').type(username)
-      cy.get('[formcontrolname=password]').type(password)
-  
-      cy.contains('Log In').click()
 
-      if (cy.get('.swal2-actions')){
-        cy.get('.swal2-actions').contains('Continue').click()
-      }
-  
-      cy.get('h3').contains('Free Content').should('be.visible');
-    })
-  
-    it('can see My Library', () => {
-      cy.get('h3').contains('My Library').should('be.visible')
-      // cy.get('h3').contains('My Library').siblings('.seeAll').click()
-    })
+    cy.get('h3').contains('Free Content').should('be.visible');
+
+  })
+
+  // describe('course content', () => {
+  //   it('can open the MagLev unit', () => {
+  //     cy.visit('https://learn.eie.org/lmsplayer/unit/zbdn5d7bd19a434fbad5b48a89b020f5', {failOnStatusCode: false})
+  //     cy.get('p').contains('Designing Maglev Systems').should('be.visible')
+  //   })
+  // })
 })
