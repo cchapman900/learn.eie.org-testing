0c3840aca1c5ff4291d49d87603d0c13f70cb12b
Chris Chapman <chrischapman900@gmail.com>
2022-01-05 14:29:16 -0500

    basic test

--- cypress/integration/learn.eie.org/login.spec.js
+++ cypress/integration/learn.eie.org/login.spec.js
@@ -1,11 +1,35 @@
 /// <reference types="cypress" />
 
-describe('example to-do app', () => {
-  beforeEach(() => {
-    cy.visit('https://learn.eie.org')
-  })
+describe('log in to dashboard', () => {
 
-  it('can log in', () => {
-    cy.get('[formcontrolname=username]').type('test')
-  })
+    before(() => {
+      cy.visit('https://learn.eie.org')
+    })
+
+    it('shows the login screen'), () => {
+      cy.get('formcontrolname=username').should('be.visible')
+    }
+  
+    it('can log in', {
+      defaultCommandTimeout: 60000
+    }, () => {
+      const username = Cypress.env('username');
+      const password = Cypress.env('password');
+  
+      cy.get('[formcontrolname=username]').type(username)
+      cy.get('[formcontrolname=password]').type(password)
+  
+      cy.contains('Log In').click()
+
+      if (cy.get('.swal2-actions')){
+        cy.get('.swal2-actions').contains('Continue').click()
+      }
+  
+      cy.get('h3').contains('Free Content').should('be.visible');
+    })
+  
+    it('can see My Library', () => {
+      cy.get('h3').contains('My Library').should('be.visible')
+      // cy.get('h3').contains('My Library').siblings('.seeAll').click()
+    })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,12 +11,17 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+require('dotenv').config()
 
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  // copy any needed variables from process.env to config.env
+  config.env.username = process.env.USERNAME
+  config.env.password = process.env.PASSWORD
+
+  // do not forget to return the changed config object!
+  return config
 }
